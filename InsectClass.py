1c6c8d3b2933dc7284fc1949f61503d32e17c2f1
Braeden Cole <braeden_cole1@baylor.edu>
2023-02-08 15:42:25 -0600

    class 02/08/23

---
+++ InsectClass.py
@@ -0,0 +1,14 @@
+import random 
+
+class Insect:
+
+    def __init__(self):
+        self.wings = 2
+        self.legs = 4
+        self.flight = None
+
+    def flight_time(self):
+        self.flight = random.randint(1,11)
+
+    def get_flight_time(self):
+        return self.flight
